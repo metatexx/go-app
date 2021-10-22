f4dbfe00eec5f4e2e32ce09fa31d18a0ad8cf715
Hans Raaf <hara@oderwat.de>
2021-10-22 11:39:23 +0200

    Implements custom tags with xml namespaces
    - Extended generate so it can also handle Elem()
    - Added a test for Elem() (elem_test.go)
    - I did not like to add SelfClosing() as method to HTMLElem when
      ElemSC() is so much easier to do.

--- pkg/app/js_nowasm.go
+++ pkg/app/js_nowasm.go
@@ -195,6 +195,10 @@ func (w *browserWindow) createElement(tag string) (Value, error) {
 	return value{}, nil
 }
 
+func (w *browserWindow) createElementNS(tag string, ns string) (Value, error) {
+	return value{}, nil
+}
+
 func (w *browserWindow) createTextNode(v string) Value {
 	return value{}
 }
